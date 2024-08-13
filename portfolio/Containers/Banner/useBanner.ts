d33ef40dae2f7b03c6374c62115ab8adabb4f235
jrummuka <jrummuka@student.hive.fi>
2024-08-13 14:43:41 +0300

    smal tweak on bannerView

--- portfolio/Containers/Banner/useBanner.ts
+++ portfolio/Containers/Banner/useBanner.ts
@@ -51,7 +51,7 @@ export const useBanner = () => {
             BannerImg1: 'I am one of the founders of amateur football club, Arkki. We are playing in 5th division in Helsinki. Project is aimed to provide low threshold exercise and community to people moving in from elsewhere.',
             BannerImg2: 'I am enthusiastic about coding and problem solving, at this moment my strongest areas are: C, ReactJS, HTML, CSS. I am also familiar with Python, TypeScript, PHP, SQL, MongoDB, NodeJS, Git, Docker, Azure, Vercel, Cypress, ETC.',
             BannerImg3: 'My interest of coding sparked while working as web-dev/it-guy on somefix, then journey in coding continued in Hive Helsinki, where I learned to code in C. I am grateful for the experience and the people I met there. I am looking forward to the next steps in my coding journey.',
-            BannerImg4: 'Football has been big part of my life since I was a kid, and my big dream would be possibility to combine these two passions. Coding and sports.',
+            BannerImg4: 'Football has been big part of my life since my youth, and my big dream would be possibility to combine these two passions. Coding and sports.',
             BannerImg5: 'I have a dog, called Niji. She is a 5 year old, rescued Husky-Malamute. Including her, every single doggo is awesome.',
             BannerImg6: 'More than developer I see myself as problem solver. I am always looking for new challenges and ways to improve my skills. Most of the languages and tools I know of I see as tools, but the main part is to know how to solve the problem.'
         };
